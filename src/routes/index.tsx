dcbf2d4cd4cd487baa5b2192c778a6f7d9917b97
jtiagosantos <tiagosan040@gmail.com>
2022-09-21 06:41:40 -0300

    feat: handle loading Data

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,19 @@ import { NavigationContainer } from '@react-navigation/native';
 //hooks
 import { useAuthState } from '../hooks/auth/useAuthState';
 
+//screens
+import { Splash } from '../screens/Splash/Splash';
+
 //routes
 import { TabRoutes } from './tabs';
 import { StackRoutes } from './stack';
 
 export const AppRoutes = () => {
-  const { user } = useAuthState();
+  const { user, loadingData } = useAuthState();
+  
+  if (loadingData) {
+    return <Splash />;
+  }
 
   return (
     <NavigationContainer>
