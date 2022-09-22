c5221c42ddd880d22a627db1ebbc8fddbfe3d4ca
jtiagosantos <tiagosan040@gmail.com>
2022-09-22 20:19:06 -0300

    chore: include 'onDeleteTransaction' property in TransactionCard type

--- src/components/TransactionCard/types.ts
+++ src/components/TransactionCard/types.ts
@@ -1,9 +1,11 @@
 export interface TransactionCardProps {
+  id: string;
   transactionType: 'inflow' | 'outflow';
   name: string;
   amount: string;
   category: string;
   date: string;
+  onDeleteTransaction: (transactionId: string) => Promise<void>;
 }
 
 export interface AmountProps {
